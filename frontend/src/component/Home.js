a3d9200869a28666b06f6aa0c8d1b636ce2342d1
Ahmad Marei <82359651+101ahmadmarei@users.noreply.github.com>
2024-06-07 15:22:38 +0300

    feat: connect python with home page

--- frontend/src/component/Home.js
+++ frontend/src/component/Home.js
@@ -1,38 +1,28 @@
-import React from 'react';
-import { PieChart, Pie, Cell, Legend } from 'recharts';
-import '../style/Home.css';
-import HomeImg from "../image/HomeImg.jpg";
-// تعريف المكون CircularChart
+import React from "react";
+import { PieChart, Pie, Cell, Legend } from "recharts";
+import "../style/Home.css";
+
 const CircularChart = () => {
-  const data = [// the values from shahd
-    { name: 'Empty spots         ', value: 40, color: '#28a745', label: 'Empty Spaces' },
-    { name: 'Filed spots      ', value: 30, color: '#ffc107', label: 'Filled Spaces' },
-    { name: 'Overdue           ', value: 30, color: '#dc3545', label: 'Overdue Payments' },
+  const data = [
+    { name: "Empty spots", value: 40, color: "#28a745", label: "Empty Spaces" },
+    {
+      name: "Filled spots",
+      value: 30,
+      color: "#ffc107",
+      label: "Filled Spaces",
+    },
+    { name: "Overdue", value: 30, color: "#dc3545", label: "Overdue Payments" },
   ];
 
-  // const chartStyle = {
-  //   margin: 'auto',
-  //   marginTop: '20px',
-  //   border: '1px solid #ccc',
-  //   borderRadius: '5px',
-  //   padding: '20px',
-  // };
-
   const legendStyle = {
-    textAlign: 'center',
-    marginTop: '10px',
+    textAlign: "center",
+    marginTop: "10px",
   };
 
   return (
-    <div className="chart-container"> {/* Apply chart container class */}
+    <div className="chart-container">
       <PieChart width={250} height={400}>
-        <Pie className='coulerd'
-          data={data}
-          // stroke="#fff"
-          // dataKey="value"
-          // innerRadius={60}
-          // outerRadius={80}
-        >
+        <Pie className="coulerd" data={data}>
           {data.map((entry, index) => (
             <Cell
               key={index}
@@ -40,22 +30,21 @@ const CircularChart = () => {
               stroke="#fff"
               dataKey="value"
               label={entry.label}
-          className={`custom-class-${index}`}
-          >
-            <text x={200} y={200} dy={8} textAnchor="middle" fill="#000" style={{marginTop: '1000px'}}>
-              {entry.name}
-            </text>
-          </Cell>
-        ))}
+              className={`custom-class-${index}`}
+            >
+              <text x={200} y={200} dy={8} textAnchor="middle" fill="#000">
+                {entry.name}
+              </text>
+            </Cell>
+          ))}
         </Pie>
-        <Legend className="legend-container" style={legendStyle} /> {/* Apply legend container class */}
+        <Legend className="legend-container" style={legendStyle} />
       </PieChart>
     </div>
   );
 };
 
-// تصدير المكون CircularChart
-export default function Home() {
+const Home = () => {
   return (
     <div>
       <section>
@@ -65,22 +54,26 @@ export default function Home() {
               <div className="header-content">
                 <div className="overlay">
                   <div className="header-content-item">
-                  
-                    <img src={HomeImg} alt="Bootstrap" />
+                    <img
+                      src="http://127.0.0.1:5001/video_feed"
+                      alt="Live Feed"
+                    />
                   </div>
                 </div>
               </div>
             </div>
             <div className="col-md-6">
-           <div>
-              <div className="pie-chart-container mt-2 rectangle shadow p-3 mb-5 bg-body-tertiary rounded">
-                <CircularChart /> {/* استخدام المكون CircularChart هنا */}
+              <div>
+                <div className="pie-chart-container mt-2 rectangle shadow p-3 mb-5 bg-body-tertiary rounded">
+                  <CircularChart />
+                </div>
               </div>
-           
-            </div> </div>
+            </div>
           </div>
         </div>
       </section>
     </div>
   );
-}
+};
+
+export default Home;
